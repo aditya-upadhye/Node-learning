e8f2cc8a5c96598004da626b4fd71261e4dbc3cf
aditya-upadhye <adityaupadhye47@gmail.com>
2022-05-24 17:57:29 +0530

    Building a very simple API

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -32,6 +32,9 @@ const url = require("url");
 ///////////////////////////////////////////////////////////
 // SERVER
 
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
   console.log(req.url);
 
@@ -41,7 +44,13 @@ const server = http.createServer((req, res) => {
     res.end("This is the OVERVIEW");
   } else if (pathName === "/product") {
     res.end("This is the PRODUCT");
-  } else {
+  } else if (pathName === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
+  }
+
+  // res.end(data);
+  else {
     res.writeHead(404, {
       "Content-type": "text/html",
       "my-own-header": "hello-world",
